96b508090b122c70f578f4e5b28e7711350e10f8
stennyyoutube <63310830+stennyyoutube@users.noreply.github.com>
2021-12-19 03:49:34 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+
+#BOT:
+TOKEN = "5051423021:AAGZH8a9aWfufGgllxErCx8gaIyfLE72Nlc" # ТОКЕН БОТА
+ADMIN = 5044167146 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # https://my.telegram.org/auth
+API_ID = 12087715
+API_HASH = "cadeadb58718021d58e920ab596269c3"
